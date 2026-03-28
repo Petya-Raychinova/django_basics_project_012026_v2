e8acc61ad785727461b8c1d2ad1f07870c2eb033
Petya Raychinova <petya@email.com>
2026-03-28 22:01:57 +0200

    Switch DB to SQLite

--- djangobasicsproject/settings.py
+++ djangobasicsproject/settings.py
@@ -87,14 +87,21 @@ load_dotenv()
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+#DATABASES = {
+#   "default": {
+#       "ENGINE": "django.db.backends.postgresql",
+#       "NAME": os.getenv('DB_NAME'),
+#       "USER": os.getenv('DB_USER'),
+#       "PASSWORD": os.getenv('DB_PASS'),
+#       "HOST": "127.0.0.1",
+#       "PORT": os.getenv('DB_PORT'),
+#   }
+#}
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASS'),
-        "HOST": "127.0.0.1",
-        "PORT": os.getenv('DB_PORT'),
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
