a8b19bd451b1bfdd35b15943f9bb2d3fe00a4211
Petya Raychinova <petya@email.com>
2026-03-30 23:10:18 +0300

    fix sendgrid sender issue

--- reports/services.py
+++ reports/services.py
@@ -32,7 +32,7 @@ def generate_report():
     email = EmailMessage(
         subject="Daily Bonus Report",
         body="В прикачения файл е дневният отчет за бонус от покупката.",
-        from_email="petya.raychinova@gmail.com",
+        from_email=None, #тест, защото е оторизиран и пак дава грешка
         to=["petya.raychinova@gmail.com"],
     )
 
