87290ce34ae40c255aa4ffa920012acf65149c31
Petya Raychinova <petya@email.com>
2026-04-04 22:59:47 +0300

    Complete Django project

--- djangobasicsproject/settings.py
+++ djangobasicsproject/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG') == "True" # False for deployment;True for development
+DEBUG = os.getenv('DEBUG') == "False" # False for deployment;True for development
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
